036bb6aa069bf9eb49571c9faa73068e3866e5d3
your_brain <144597205+Your-Brain@users.noreply.github.com>
2024-07-23 23:23:08 +0530

    old
    old

---
+++ brain.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".typing", {
+  strings: ["Hii!", " I'am", "Partha ", "Batabyal"],
+  typeSpeed: 80,
+  BackSpeed: 20,
+  loop: true,
+  showCursor: false,
+});
+
+var typed = new Typed(".typingLocation", {
+  strings: ["West Bengal", "purba Medinipur", "Panskura", "Talitya"],
+  typeSpeed: 80,
+  BackSpeed: 20,
+  loop: true,
+  showCursor: false,
+});
